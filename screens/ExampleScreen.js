e3eabdd317a089d637d881ea4d38771173707ea8
Chester Wong <psyforce99@gmail.com>
2022-06-10 00:18:47 +0800

    Create document works

--- screens/ExampleScreen.js
+++ screens/ExampleScreen.js
@@ -1,64 +1,58 @@
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
 import React, { useState } from 'react'
-import { firestore as db } from "@react-native-firebase/firestore"
+import firestore from "@react-native-firebase/firestore"
 
 // We're performing CRUD on a single firestore document.
 const exampleCollectionName = 'ExampleCollection'
 const exampleDocumentName = 'ExampleDocument'
-const exampleDocReference = db.collection(exampleCollectionName).doc(exampleDocumentName)
 const ExampleScreen = () => {
+  const ref = firestore().collection(exampleCollectionName)
   const [exampleDoc, setExampleDoc] = useState(null)
   const [text, setText] = useState('')
   // MARK: react-native-firebase/firestore v6 example CRUD functions
-  const Create = () => {
+  async function Create() {
     const docData = {
       'string-field-1': 'placeholder text',
       'num-field-1': 1,
       'read-me': 'Hello World!',
     }
-    exampleDocReference.set(docData)
-      // NOTE: This is a promise (that's why you should use typescript, then its clear :] )
-      .then(() => {
-        alert('Document Created!')
-      })
-      .catch(error => {
-        alert(error.message)
-      })
+    await ref.add(docData)
+    alert('Document Created!')
   }
   const Read = () => {
     // Read whatever document by changing the target collection and path here.
     // You can also read entire collections: https://firebase.google.com/docs/firestore/quickstart#read_data
-    exampleDocReference.get()
-      .then((snapshot) => {
-        if (snapshot.exists) {
-          setExampleDoc(snapshot.data())
-        } else {
-          alert('Document Not Found.')
-        }
-      })
-      .catch(error => {
-        alert(error.message)
-      })
+    // exampleDocReference.get()
+    //   .then((snapshot) => {
+    //     if (snapshot.exists) {
+    //       setExampleDoc(snapshot.data())
+    //     } else {
+    //       alert('Document Not Found.')
+    //     }
+    //   })
+    //   .catch(error => {
+    //     alert(error.message)
+    //   })
   }
   const Update = (value, merge) => {
-    exampleDocReference.set( value, { merge: merge })
-    // If merge is true then it will merge with an existing doc, otherwise it will be a fresh one.
-      .then(() => {
-        alert('Updated Successfully!')
-        setText('')
-      })
-      .catch(error => {
-        alert(error.message)
-      })
+    // exampleDocReference.set( value, { merge: merge })
+    // // If merge is true then it will merge with an existing doc, otherwise it will be a fresh one.
+    //   .then(() => {
+    //     alert('Updated Successfully!')
+    //     setText('')
+    //   })
+    //   .catch(error => {
+    //     alert(error.message)
+    //   })
   }
   const Delete = () => {
-    exampleDocReference.delete()
-      .then(() => {
-        alert('Deleted Successfully!')
-      })
-      .catch(error => {
-        alert(error.message)
-      })
+    // exampleDocReference.delete()
+    //   .then(() => {
+    //     alert('Deleted Successfully!')
+    //   })
+    //   .catch(error => {
+    //     alert(error.message)
+    //   })
   }
   return (
     <View style={styles.container}>
